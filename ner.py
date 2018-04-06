d760bec759836c1e56f6351b5cccc09efddc4790
nhatnv <nguyenvannhat152@gmail.com>
2018-04-07 02:34:51 +0700

    20 epoch

--- ner.py
+++ ner.py
@@ -24,6 +24,6 @@ embeddings = np.load(EMBEDDING_PATH)
 vocabs = json.load(open(VOCAB_PATH, "r", encoding="utf8"))
 
 # Use pre-trained word embeddings
-model = anago.Sequence(max_epoch=50, embeddings=embeddings, batch_size=100, early_stopping=False, vocab_init=vocabs)
+model = anago.Sequence(max_epoch=20, embeddings=embeddings, batch_size=100, early_stopping=False, vocab_init=vocabs)
 model.train(x_train, y_train, x_valid, y_valid)
 model.eval(x_test, y_test)
