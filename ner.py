2d08b44b11968be56afd57a7a62581baf7bfd81d
nhatnv <nguyenvannhat152@gmail.com>
2018-04-07 11:29:01 +0700

    batch size 20

--- ner.py
+++ ner.py
@@ -19,11 +19,12 @@ x_valid, y_valid = load_data_and_labels(valid_path)
 x_test, y_test = load_data_and_labels(test_path)
 print(len(x_train), 'train sequences')
 print(len(x_valid), 'valid sequences')
+print(len(x_test), 'test sequences')
 
 embeddings = np.load(EMBEDDING_PATH)
 vocabs = json.load(open(VOCAB_PATH, "r", encoding="utf8"))
 
 # Use pre-trained word embeddings
-model = anago.Sequence(max_epoch=20, embeddings=embeddings, batch_size=100, vocab_init=vocabs)
+model = anago.Sequence(max_epoch=20, embeddings=embeddings, vocab_init=vocabs)
 model.train(x_train, y_train, x_valid, y_valid)
 model.eval(x_test, y_test)
