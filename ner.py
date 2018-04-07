0335c9265a028bf531a59adae2a677cb1228ff69
nhatnv <nguyenvannhat152@gmail.com>
2018-04-07 11:27:29 +0700

    pos embed auto

--- ner.py
+++ ner.py
@@ -6,8 +6,8 @@ import numpy as np
 import anago
 from anago.reader import load_data_and_labels
 
-VOCAB_PATH = 'embedding/vocabs_2.json'
-EMBEDDING_PATH = 'embedding/word_embeddings_2.npy'
+VOCAB_PATH = 'embedding/vocabs.json'
+EMBEDDING_PATH = 'embedding/word_embeddings.npy'
 
 train_path = "data/train/Doi_song.muc"
 valid_path = "data/dev/Doi_song.muc"
@@ -24,6 +24,6 @@ embeddings = np.load(EMBEDDING_PATH)
 vocabs = json.load(open(VOCAB_PATH, "r", encoding="utf8"))
 
 # Use pre-trained word embeddings
-model = anago.Sequence(max_epoch=20, embeddings=embeddings, batch_size=100, early_stopping=False, vocab_init=vocabs)
+model = anago.Sequence(max_epoch=20, embeddings=embeddings, batch_size=100, vocab_init=vocabs)
 model.train(x_train, y_train, x_valid, y_valid)
 model.eval(x_test, y_test)
