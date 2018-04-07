c07aa4cea985df4f4b6790622e6d98e358b47fbe
nhatnv <nguyenvannhat152@gmail.com>
2018-04-07 13:39:24 +0700

    .

--- anago/models.py
+++ anago/models.py
@@ -56,15 +56,16 @@ class SeqLabeling(BaseModel):
                                         mask_zero=True,
                                         weights=[embeddings])(word_ids)
 
-        # pos_embed_weights = [
-        #     np.zeros([1, config.pos_vocab_size-1]),  # padding
-        #     np.identity(config.pos_vocab_size-1)
-        # ]
-        # pos_embed_weights = np.concatenate(pos_embed_weights)
+        pos_embed_weights = [
+            np.zeros([1, config.pos_vocab_size-1]),  # padding
+            np.identity(config.pos_vocab_size-1)
+        ]
+        pos_embed_weights = np.concatenate(pos_embed_weights)
         pos_ids = Input(batch_shape=(None, None), dtype='int32')
-        pos_embeddings = Embedding(input_dim=config.pos_vocab_size,
-                                    output_dim=config.pos_vocab_size - 1,
-                                    mask_zero=True,)(pos_ids)
+        pos_embeddings = Embedding(input_dim=pos_embed_weights.shape[0],
+                                    output_dim=pos_embed_weights.shape[1],
+                                    mask_zero=True,
+                                    weights=[pos_embed_weights])(pos_ids)
 
         # build character based word embedding
         char_ids = Input(batch_shape=(None, None, None), dtype='int32')
