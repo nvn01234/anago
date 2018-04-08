1c0405d55f6e48005cb5ea8db1f960acea79b8bd
nhatnv <nguyenvannhat152@gmail.com>
2018-04-08 12:29:02 +0700

    fix

--- anago/trainer.py
+++ anago/trainer.py
@@ -27,6 +27,7 @@ class Trainer(object):
 
         kb_words = self.preprocessor.transform_kb(kb_words)
         kb_avg = self.kb_miner.predict(kb_words)
+        print(kb_avg.shape)
 
         # Prepare training and validation data(steps, generator)
         train_steps, train_batches = batch_iter(x_train, kb_avg,
